b187c9fd359e2f30a66f823a1413dbb6538a1330
viniciuslimaLEDA <00001115234262sp@al.educacao.sp.gov.br>
2024-09-30 20:56:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,18 @@
-function setup() {
+function setup() 
+{
   createCanvas(400, 400);
+  background(220);
 }
 
-function draw() {
-  background("white");
-  fill("black");
-  textSize(64);
-  textAlign(CENTER,CENTER);
+function draw() 
+{
+  rect(0,0,40,50);
+  stroke("blue");
+  strokeWeight(5);
+  fill("red")
   
-  let maximo = width;
-  let minimo = 0;
-  //mouseX, 0, width ==> 0, palavra.length
-  let palavra = "Vinicius";
-  let quantidade = map(mouseX, 0, width, 1, palavra.length);
-  //console.log(quantidade);
-  let parcial = palavra.substring(0,quantidade);
-  text(parcial,200,200);
-  
-  // if(mouseX <50) {
-  // let palavra = "C";
-  //text(palavra,200,200);
-  //}else if (mouseX <100){
-  //let palavra = "Ca";
-  // text(palavra,200,200);
-  //}else{
-  //let palavra = "Caminhante";
-  // text (palavra,200,200);
-  //}
-
-
-  
+  if(mouseIsPressed)
+  {
+  rect(mouseX, mouseY, 20, 35);
+   } 
 }
